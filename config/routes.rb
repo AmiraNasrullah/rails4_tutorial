e4f1fa52813c2c36e288fe2bee7757005062d070
amira <amira@amira-HP-G62-Notebook-PC.(none)>
2014-08-03 17:18:50 +0200

    Sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   match '/home', to: 'static_pages#home', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
+  resources :users
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
